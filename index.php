f14c1027c73925879d723a28fbb248abe8e565d3
Ricardo Cesar <rcesarh@uol.com.br>
2019-01-18 19:07:40 -0200

    manutençao do administrador

--- index.php
+++ index.php
@@ -57,6 +57,96 @@ $app->get('/admin/logout', function(){
 
 });
 
+$app->get('/admin/users', function(){
+
+	User::verifyLogin();
+
+	$users = User::listAll();
+
+	$page = new PageAdmin();
+
+    $page->setTpl("users",array(
+    		"users"=>$users
+    ));
+
+});
+
+$app->get('/admin/users/create', function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+    $page->setTpl("users-create");
+
+});
+
+$app->get('/admin/users/:iduser/delete', function($iduser){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+    $page->setTpl("users-update");
+
+});
+
+$app->get('/admin/users/:iduser', function($iduser){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+
+	$page = new PageAdmin();
+
+    $page->setTpl("users-update", array(
+    	'user'=>$user->getValues()
+    ));
+
+});
+
+$app->post('/admin/users/create', function(){
+
+	User::verifyLogin();
+
+	$user = new User();
+	$user->setData($_POST); /// aqui ele cria os seters e geters
+	$user->save();
+
+	header("Location: /admin/users");
+	exit;
+	
+
+});
+
+$app->post('/admin/users/:iduser/delete', function($iduser){
+
+	User::verifyLogin();
+
+});
+
+$app->post('/admin/users/:iduser', function($iduser){
+
+	User::verifyLogin();
+
+	$user =  new User();
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+
+});
+
+
+
+
 
 $app->run();
 
